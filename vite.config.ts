3ab9d3bd04fdb6d50381d3f29c9c0eca4021198e
Admin <admin@Admins-MacBook-Pro.local>
2025-03-10 13:38:50 +0530

    made sure npm build works correctly to deploy to netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { PostgrestClient } from '@supabase/postgrest-js';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,6 +16,8 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    commonjs(),
+    resolve(),
   ].filter(Boolean),
   resolve: {
     alias: {
