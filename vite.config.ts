4a947143ff3a380b60ec892bbd8d1a19a005aa5b
Admin <admin@Admins-MacBook-Pro.local>
2025-03-10 13:46:30 +0530

    updated server[] in vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import { PostgrestClient } from '@supabase/postgrest-js';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import { PostgrestClient } from '@supabase/postgrest-js';
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    mimeTypes: {
+      'application/javascript': ['js', 'jsx', 'ts', 'tsx'],
+    }
   },
   plugins: [
     react(),
